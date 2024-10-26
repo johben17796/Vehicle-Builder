e262a4e29f127fa99d33420c2ec4b022b7e0aac0
Ben Johnson <johben17796@gmail.com>
2024-10-26 12:33:47 -0500

    Got some good progress, only really the CLI stuff left

--- Develop/src/classes/Truck.ts
+++ Develop/src/classes/Truck.ts
@@ -43,13 +43,23 @@ class Truck extends Vehicle implements AbleToTow {
   }
   // TODO: Implement the tow method from the AbleToTow interface
   tow(vehicle: Truck | Motorbike | Car): void {
+    const truckMake = this.make;
+    const truckModel = this.model;
     // TODO: Get the make an model of the vehicle if it exists
     // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
     // TODO: If it is, log that the vehicle is being towed
     // TODO: If it is not, log that the vehicle is too heavy to be towed
+      if (this.weight <= this.towingCapacity) {
+        console.log(`The ${truckMake} ${truckModel} is being towed`)
+      } else {
+        console.log(`The ${truckMake} ${truckModel} is too heavy to be towed`)
+      }
   }
 
   // TODO: Override the printDetails method from the Vehicle class
+  override printDetails(): void {
+    console.log(this.vin)
+  }
     // TODO: The method should call the printDetails method of the parent class
     // TODO: The method should log the details of the Truck
     // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
